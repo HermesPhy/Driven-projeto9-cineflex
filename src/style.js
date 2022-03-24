e5301769fbd30a5dbf22dd948f0111a5a2bb7758
Hermes Júnior <hermesj.junior@gmail.com>
2022-03-24 14:36:16 -0300

    Preparação para o desenvolvimento

---
+++ src/style.js
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from "styled-components";
+
+export const EstiloCorpo = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 'Recursive', sans-serif;
+        font-weight: 400;
+        background-color: #FB6B6B;
+        cursor: default;
+    }
+`
